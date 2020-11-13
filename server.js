6585b9f323601a02057a270428c59603e3db0627
YoungAncient <68391309+YoungAncient@users.noreply.github.com>
2020-11-13 09:55:00 -0500

    added middleware, added provided db and public folders

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const express = require('express');
 
 // create instance of app and declare port
 const app = express();
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
+
+
+// declare middleware required for parsing incoming Request Object as a JSON Object
+app.use(express.json());
+// decalre middleware required for recognizing incoming Request Object as and Array or String
+app.use(express.urlencoded({ extended: true}));
+// serve static files
+app.use(express.static('public'));
+
 
 
 
